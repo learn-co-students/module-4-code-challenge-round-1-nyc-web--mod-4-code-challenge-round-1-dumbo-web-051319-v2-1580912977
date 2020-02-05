9fe27b6aca01a9bc6025d517b888d733da89a996
Shakil Abdin <shakil.abdin@gmail.com>
2020-02-05 09:42:59 -0500

    1st and 2nd deliverables

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
+let API = "http://localhost:3005/books"
+
 class App extends Component {
+
+
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    fetch(API).then(resp => resp.json())
+    .then(result => this.setState({
+      books: result
+    }))
+  }
+
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
+        <BookList books={this.state.books} />
         <Bookshelf />
       </div>
     );
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,12 +3,17 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
+
+  renderBooks = () => {
+    return this.props.books.map(book => <li><Book key={book.id} {...book}/></li>)
+  }
+
   render() {
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{this.renderBooks()}</ul>
       </div>
     );
   }
